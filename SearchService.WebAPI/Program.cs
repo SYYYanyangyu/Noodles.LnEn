541b9f298aee0b93b2b90dbaf5d553b871a9fad2
SYYYanyangyu <1245938403@qq.com>
2023-11-30 20:34:07 +0800

    add trycatch

--- SearchService.WebAPI/Program.cs
+++ SearchService.WebAPI/Program.cs
@@ -1,25 +1,33 @@
-var builder = WebApplication.CreateBuilder(args);
+using CommonInitializer;
+using SearchService.Infrastructure;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.ConfigureDbConfiguration();
+builder.ConfigureExtraServices(new InitializerOptions
+{
+    LogFilePath = "e:/temp/SearchService.log",
+    EventBusQueueName = "SearchService.WebAPI"
+});
 // Add services to the container.
 
+builder.Services.Configure<ElasticSearchOptions>(builder.Configuration.GetSection("ElasticSearch"));
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "SearchService.WebAPI", Version = "v1" });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (builder.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SearchService.WebAPI v1"));
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
+app.UseDefault();
 app.MapControllers();
 
 app.Run();
