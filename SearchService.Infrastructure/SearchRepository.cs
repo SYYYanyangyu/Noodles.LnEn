57787de70c8efff11c5547077ef970d881ab59fc
shiyangyang <1245938403@qq.com>
2023-12-03 19:06:48 +0800

    elasticseach 创建索引

--- SearchService.Infrastructure/SearchRepository.cs
+++ SearchService.Infrastructure/SearchRepository.cs
@@ -1,11 +1,5 @@
 ﻿using Nest;
 using SearchService.Domain;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 using Noodles.Common;
 
 namespace SearchService.Infrastructure
